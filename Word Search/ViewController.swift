30e73c2b25c70a35023a619fec8247618512d29b
chris lekkas <chris.lekkas@fiserv.com>
2019-02-17 10:07:56 -0800

    Initial Commit

---
+++ Word Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Search
+//
+//  Created by Lekkas, Chris (Hillsboro) on 2/17/19.
+//  Copyright © 2019 Lekkas, Chris (Hillsboro). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
